5cd5a233ec535c67adf02c3e5dd874410e98b984
Christiaan Slabbert <21810354@sun.ac.za>
2022-08-12 02:08:39 +0200

    update scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,8 @@
  #!/bin/bash
-parent_path=$( cd "$(dirname "${BASH_SOURCE[1]}")" ; pwd -P )
-cd "$parent_path"
+
+SCRIPT_RELATIVE_DIR=$(dirname "${BASH_SOURCE[0]}")
+cd $SCRIPT_RELATIVE_DIR
+cd ..
+
 . venv/bin/activate
 python3 main.py
